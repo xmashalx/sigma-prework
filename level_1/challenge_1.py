4cf09713336cdcb4065c975b76761859935f84f8
xmashalx <Mashalchr@gmail.com>
2025-10-15 11:05:55 +0100

    Update challenge_1.py

--- level_1/challenge_1.py
+++ level_1/challenge_1.py
@@ -1,2 +1,8 @@
+name=input('what is you name: ')
+authorised_names=['Alice','Bob']
+if name in authorised_names:
+  print(f"Hello {name}!")
+else:
+  print(f'sorry... your not authroised to be greeted!')
 
 
