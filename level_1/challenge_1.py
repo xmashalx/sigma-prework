14866f62c0adcd86f80b57e580227d6d19635ac8
xmashalx <Mashalchr@gmail.com>
2025-10-15 11:12:31 +0100

    Delete level_1/challenge_1.py

--- level_1/challenge_1.py
+++
@@ -1,8 +0,0 @@
-name=input('what is you name: ')
-authorised_names=['Alice','Bob']
-if name in authorised_names:
-  print(f"Hello {name}!")
-else:
-  print(f'sorry... your not authroised to be greeted!')
-
-
